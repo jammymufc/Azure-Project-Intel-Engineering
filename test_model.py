5c174719ae24cedd744033d2f8113480568270ae
Jamie Maguire <50004736@serc.ac.uk>
2024-12-11 11:08:04 +0000

    Renamed file and small changes before staging test

--- test_model.py
+++ test_model.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 import numpy as np
-from train import loadData, splitData, buildModel, assessModel
+from model import loadData, splitData, buildModel, assessModel
 from sklearn.ensemble import RandomForestClassifier
 
 class TestTrain(unittest.TestCase):
